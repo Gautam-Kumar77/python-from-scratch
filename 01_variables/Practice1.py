5fc0f1f98e3b0112288bdc9e233f9fe671c141d7
Gautam Gupta <imgautii5177@gmail.com>
2026-08-05 22:50:58 +0530

    Practice question on Variables

---
+++ 01_variables/Practice1.py
@@ -0,0 +1,39 @@
+''' Q.1 Addition
+
+Create two variables:
+num1 = 15
+num2 = 25
+Store their sum in another variable and print it. '''
+
+num1= 15
+num2=25
+sum= num1+num2
+print(sum)
+
+''' Q2. Rectangle Area
+Store:
+Length = 15
+Width = 8
+Calculate: Area
+Print the result. '''
+
+Length= 15
+Width = 8
+Area= Length*Width
+print(Area)
+
+''' Q3. Temperature
+Store temperature in Celsius.
+Convert it to Fahrenheit.'''
+
+Celsius = 25
+Fahrenheit = (Celsius * 9/5) + 32
+print(Fahrenheit)
+
+# Q4, Swap values without third variable
+
+a= 10
+b= 5
+a , b = b, a
+print(a,b)
+
