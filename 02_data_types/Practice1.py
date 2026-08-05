078efeb01efc73e936c11cddc4dcc911ba54ce68
Gautam Gupta <imgautii5177@gmail.com>
2026-08-05 23:19:43 +0530

    Completed python data types with examples

---
+++ 02_data_types/Practice1.py
@@ -0,0 +1,20 @@
+# Q1.Convert an integer to a float.
+# Q2.Convert a float to an integer.
+# Q3.Convert a string "500" into an integer.
+# Q4.Convert the integer 25 into a string.
+
+#Q1
+a = 23
+print(float(a))
+
+#Q2
+b = 23.3
+print(int(b))
+
+#Q3
+c= "500"
+print(int(c))
+
+#Q4
+d = 25
+print(str(d))
---
+++ 02_data_types/data_types.py
@@ -0,0 +1,13 @@
+
+a = 12
+b= 34.3
+c= "Hello"
+d= 3 + 7j
+is_bool = True
+print(a,b,c,d,is_bool)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(is_bool))
+print(type(d))
