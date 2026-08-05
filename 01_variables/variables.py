ff68e7e167f47be591bf7e1b06b43fb4f8ac3f0e
Gautam Gupta <imgautii5177@gmail.com>
2026-08-05 22:22:16 +0530

    Implement variable declaration examples

---
+++ 01_variables/variables.py
@@ -0,0 +1,21 @@
+#Rules for Naming variables
+Name= "Gautam"
+_Age= 23
+Weight1 = 63.5
+
+print(Name)
+print(_Age)
+print(Weight1)
+
+#Multiple Variable Assignment
+a,b,c = 23, "Hii", 23.4
+x=y=z=100
+
+print(a,b,c)
+print(x,y,z)
+
+#Dynamic Typing
+t= 12
+t= 34.5
+t= "Hello"
+print(t)
