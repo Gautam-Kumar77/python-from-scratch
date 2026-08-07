817b404ca557594f7e1564ccd45d28f57e7d3691
Gautam Gupta <imgautii5177@gmail.com>
2026-08-07 19:26:49 +0530

    practice on float data type

---
+++ 02_data_types/float_practice.py
@@ -0,0 +1,22 @@
+a = 12.0
+b= 2.5
+c= a+b
+print(c, type(c))
+
+d= 2.0
+e= 5
+f= d+e
+print(f, type(f))
+
+#Mantisa e Exponent
+num1= 3e2
+print(num1, type(num1))
+
+num2= 8e-2
+print(num2, type(num2))
+
+num3= 0.000000000000000003
+print(num3)
+
+num4 = 0.000000000000000000000000000000000023
+print(num4,type(num4))
