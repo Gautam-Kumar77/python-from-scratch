5f6cc0b1143df7c94f64bb0de9f3d0014daaa226
Gautam Gupta <imgautii5177@gmail.com>
2026-08-08 16:20:20 +0530

    Pracice on Complex data type

---
+++ 02_data_types/complex_practice.py
@@ -0,0 +1,26 @@
+a= 3+ 5j
+print(a, type(a))
+
+b= 5-2j
+print(b, type(b))
+
+c= 3.3+ 4J
+print(c.real)
+print(c.imag)
+
+d= 5+2j
+e= 2+1j
+sum= d+e
+print(sum, type(sum))
+
+
+print(complex(2,4))
+
+z1 = 5 + 0j
+print(z1.real , type(z1))
+print(z1.imag , type(z1))
+
+z2 = 3j
+print(z2.real)
+print(z2.imag)
+print(z2.imaginary)
