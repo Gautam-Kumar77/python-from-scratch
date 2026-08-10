6c3bf5fd8b0bf07de98083974f8d1a61dc98aff7
Gautam Gupta <imgautii5177@gmail.com>
2026-08-10 18:17:50 +0530

    Learnt and practice String Data type

---
+++ 02_data_types/str_practice.py
@@ -0,0 +1,21 @@
+#Single Line String Data
+s1= "Python"
+print(s1, type(s1))
+s1= 'Python'
+s2= 'A'
+s3= "123123"
+s4= "addr23"
+s5="@#987"
+print(s1,s2,s3,s4,s5)
+
+singleLine= """Good Morning, Jarvis"""
+print(singleLine, type(singleLine))
+
+#Multi Line String Data
+multiLine= ('''Guido Van Rossum,
+            HouseNo:- 12, Banjara Hills,
+            Hyderabad, India,
+            500034'''
+            )
+print(multiLine, '\n', type(multiLine))
+
