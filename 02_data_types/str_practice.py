776765c84c335f97e8e7678e101209e19557be5b
Gautam Gupta <imgautii5177@gmail.com>
2026-08-11 16:54:05 +0530

    Practiced on indexing and slicing

--- 02_data_types/str_practice.py
+++ 02_data_types/str_practice.py
@@ -19,3 +19,42 @@ multiLine= ('''Guido Van Rossum,
             )
 print(multiLine, '\n', type(multiLine))
 
+
+# String Indexing and Slicing
+
+#INDEXING
+st= "PYTHON"
+print(st, type(st))
+print(st[True])
+print(st[False])
+print(st[-True])
+print(st[4])
+print(st[-len(st)])
+# print(st[len(st)]) IndexError
+# print(st[2.4]) TypeError
+# print(st[6]) IndexError
+
+#SLICING
+
+# CASE 1. +ve BEGIN: +ve END
+st1 = "PYTHON"
+print(st1[1:4])
+print(st1[2:5])
+print(st1[5:1]) # Space/Empty
+
+# CASE 2. -ve BEGIN: -ve END
+print(st1[-5:-2])
+print(st1[-6: -1])
+print(st1[-1:-5]) # Space/Empty
+
+#CASE 3. +ve BEGIN: -ve END
+print(st1[1:-1])
+print(st1[2:-3])
+print(st1[5:-2]) # Space/Empty
+
+#CASE 4. -ve BEGIN: +ve END
+print(st1[-6:4])
+print(st1[-3:5])
+print(st1[-4:4])
+print(st1[-6:6])
+
