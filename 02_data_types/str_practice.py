d7037af2119182aaea44d65209c7bd05083e72b3
Gautam Gupta <imgautii5177@gmail.com>
2026-08-13 17:28:14 +0530

    Practice slicing concept

--- 02_data_types/str_practice.py
+++ 02_data_types/str_practice.py
@@ -36,6 +36,10 @@ print(st[-len(st)])
 
 #SLICING
                                              #Syntax 1
+
+#This Syntax Generates Range of Characters OR Sub String from BEGIN Index to END-1 Index in FORWARD Direction with
+# Default Step +1  provided  BEGIN < END Index Otherwise we get Space OR ' ' as Result.
+
 # CASE 1. +ve BEGIN: +ve END
 st1 = "PYTHON"
 print(st1[1:4])
@@ -59,6 +63,7 @@ print(st1[-4:4])
 print(st1[-6:6])
 
                                                       #SYNTAX 2
+#In this Syntax, We are Specifying the BEGIN Index and Not Specifying END Index.
 
 #strObj[BEGIN: ]
 s= "PYTHON"
@@ -70,6 +75,7 @@ print(s[-5:])
 print(s[-1000:])
 
                                              #Syntax 3
+#In this Syntax, We are Specfying the END Index and Not Specifying  BEGIN Index.
 
 #strObj[ :END]
 print(s[: 4])
@@ -78,6 +84,7 @@ print(s[:-2])
 print(s[: -4])
 
                                             #Syntax 4
+#In this Syntax, We are Not Specfying Both BEGIN and  END Indices
 
 #strObj[:]
 print(s[:])
@@ -85,25 +92,74 @@ addr= "Hyderabad"
 print(addr[:])
 
                                             #Syntax 5
+#
 #strObj[BEGIN:END:STEP]
 
-#Rule 1. +ve BEGIN: +ve END : +ve STEP
+#Rule 2. If the Value of STEP is +VE then PVM Takes of Range of Chars from BEGIN Index to END-1 Index in FORWARD
+        #Direction  by Maintaining the Value of STEP provided BEGIN < END Otherwise we get Sapce(' ') as Result
+
+# +ve BEGIN: +ve END : +ve STEP
 print(s[1:6:1])
 print(s[0:5:2])
 print(s[1:6:1])
 print(s[0:6:4])
 
-#Rule 2. -ve BEGIN: -ve END : +ve STEP
+# -ve BEGIN: -ve END : +ve STEP
 print(s[-6:-1:1])
 print(s[-5:-2:2])
 print(s[-2:-4:3])  #Empty/Space
 
-#Rule 3. +ve BEGIN: -ve END : +ve STEP
+#  +ve BEGIN: -ve END : +ve STEP
 print(s[1:-1:2])
 print(s[2:-2:2])
 print(s[3:-3:3])   #Empty/Space
 
-#Rule 4. -ve BEGIN: +ve END : +ve STEP
+# -ve BEGIN: +ve END : +ve STEP
 print(s[-6:6:1])
 print(s[-5:5:3])
 print(s[-100:100:4])
+
+#Rule 3. If the Value of STEP is -VE then PVM Takes Range of Chars in BACKWARD Direction  from BEGIN Index to END+1
+         #Index by Maintaining the Value of STEP Provided BEGIN > END Index Otherwise we get Sapce(' ') as Result.
+
+# +ve BEGIN: +ve END : -ve STEP
+print(s[5:0:-1])
+print(s[5:2:-2])
+print(s[4:1:-3])
+
+#-ve BEGIN: -ve END : -ve STEP
+print(s[-1:-7:-1])
+print(s[-2:-7:-3])
+print(s[-2:-100:-2])
+
+#+ve BEGIN: -ve END : -ve STEP
+print(s[4:-6:-2])
+print(s[100:-100:-1])
+print(s[5:-4:-2])
+
+#-ve BEGIN: +ve END : -ve STEP
+print(s[-1:2:-1])
+print(s[-2:2:-2])
+print(s[-4:0:-1])
+
+#We can Reverse a string by using extended slicing concept
+hyd= "HYDERABAD"
+print(hyd[-1:-10:-1])
+
+py= "PYTHON"
+print(py[-1:-100:-1])
+print(py[100:-100:-1])
+
+#We can also find Palindrome by using extended slicing concept
+m= "MADAM"
+print(m[-1:-6:-1])
+n= "NAMAN"
+print(n[100:-100:-1])
+
+#Rule 4 When we extract the data in forward direction, if we specify END INDEX 0 then we will get ''(empty/space)
+print(s[5:0:1])
+print(s[3:0:1])
+
+#Rule 5 When we extract the data in backward direction, if we specify END INDEX -1 then we will get ''(empty/space)
+print(s[2:-1:-1])
+print(s[5:-1:-2])
