edde1ca9fd440260b900b56a1b009b0dc2eb5a91
Gautam Gupta <imgautii5177@gmail.com>
2026-08-18 20:52:08 +0530

    learned the concept on range data types

---
+++ 02_data_types/range_practice.py
@@ -0,0 +1,65 @@
+# r= range(6)
+# print(r, type(r))
+
+# for i in r:
+#     print(i)
+
+# #Syntax 1
+# for p in range(6):
+#     print(p)
+
+# for s in range(10):
+#     print(s)
+
+# for v in r[0:3]:
+#     print(v)
+
+#Syntax 2
+
+# for v in range(10, 20):
+#     print(v)
+
+#Syntax 3:
+# for r in range(10,20,3):
+#     print(r)
+
+# Q1.  0  1   2   3  4   5   6   7   8  9    range(10)  OR range(0,10)  OR range(0,10,1)
+# s= range(10)
+# for i in s:
+#     print(i)
+
+# s1= range(0,10)
+# for v in s1:
+#     print(v)
+
+# s1= range(0,10,1)
+# for v1 in s1:
+#     print(v1)
+
+# for i1 in range(10):
+#     print(i1)
+
+# for i1 in range(6):
+#     print(i1)
+
+# for i3 in range(10):
+#     print(i3)
+
+# Q2.   10   11   12   13  14  15        range(10,16)  OR range(10,16,1)
+# for i in range(10, 16):
+#     print(i)
+
+# for i in range(10,16,1):
+#     print(i)
+
+# Q3.    -1   -2   -3   -4   -5   -6  -7   -8   -9
+
+# for i in range(-1, -10, -1):
+#     print(i)
+# for i in range(-9, 0, 1):
+#     print(i)                      # it will print reverse
+
+# for i in range(-1, -10):
+#     print(i)               #No Output
+
+
