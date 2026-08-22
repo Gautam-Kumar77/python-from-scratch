453281beec4f2e08c7b2636fab6adf8a674e375f
Gautam Gupta <imgautii5177@gmail.com>
2026-08-22 09:04:53 +0530

    practice different type of syntaxes in list data type

--- 04_list_category_data_type/list_practice.py
+++ 04_list_category_data_type/list_practice.py
@@ -49,3 +49,57 @@ print(lst4)
 # Syntax-6:		ListObj= [Non-Iterable-Object]
 l= [12]
 print(l)
+
+#PreDefined functions in List
+
+#1. append()
+lst4= [34,8+2j,3.46,"PYTHON", True]
+print(lst4, type(lst4), id(lst4))
+lst4.append(False)
+lst4.append("Hyderabad")
+print(lst4, type(lst4), id(lst4))
+
+#2 extend() used to add multiple elements at the end
+lst4= [34,8+2j,3.46,"PYTHON", True]
+lst4.extend([101,102])
+print(lst4)
+
+#3 insert()  used to add the value at the specified index
+lst4= [34,8+2j,3.46,"PYTHON", True]
+lst4.insert(2, 100)
+print(lst4)
+
+lst4.insert(-2, "Hello")   # insert "Hello before the element at index -2"
+print(lst4)
+
+lst4.insert(100, 21.2)
+print(lst4)
+
+#4 remove()   used to remove first occurence of specified value from list object
+lst4= [34, (8+2j), 100, 3.46, 'Hello', 'PYTHON', True, 21.2]
+lst4.remove(21.2)
+print(lst4)
+
+l= [10,20,10,10,20,30,30,10,20]
+l.remove(30)
+print(l)
+
+# l.remove(100)
+# print(l)  #ValueError: list.remove(x): x not in list
+
+#5 pop() Based on index(used for removing the value from the list based on index)
+l= [10,20,10,10,20,30,30,10,20]
+l.pop(-2)
+print(l)
+# l.pop(100)  #IndexError: pop index out of range
+# list().pop(0)  #IndexError: pop from empty list
+# [].pop(-1) #IndexError: pop from empty list
+
+l.pop() # it will remove last element from a list
+print(l)
+
+#6 clear()  it used to remove all the elements from list
+l= [10,20,10,10,20,30,30,10,20]
+l.clear()
+print(l)
+
