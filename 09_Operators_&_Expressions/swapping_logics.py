9cf0a974146c00297e9dbc3ce2e97ac0b0c742c5
Gautam Gupta <imgautii5177@gmail.com>
2026-09-07 23:16:28 +0530

    Added practice on different methods of swapping

---
+++ 09_Operators_&_Expressions/swapping_logics.py
@@ -0,0 +1,28 @@
+#There are four ways/methods/logic to swap the number
+
+#Method 1
+a=10
+b=20
+a,b=b,a
+print("Value of a= {} \nValue of b= {} " .format(a,b))
+
+#Method 2
+a=10
+b=20
+
+print("="*40)
+print("Value of a= {} \nValue of b= {} " .format(a,b))
+temp=a
+a=b
+b=temp
+print("Values after Swapping")
+print("Value of a= {} \nValue of b= {} " .format(a,b))
+
+#Method 3
+a=2
+b=3
+
+a= a+b
+b= a-b
+a= a-b
+print("Value of a= {} \nValue of b= {} " .format(a,b))
