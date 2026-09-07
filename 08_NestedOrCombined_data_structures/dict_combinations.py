31cf75d5839e13b415eabc77dc3a80cd37bd9d28
Gautam Gupta <imgautii5177@gmail.com>
2026-09-07 20:36:28 +0530

    dict Combinations with list,tuple,set,dict

---
+++ 08_NestedOrCombined_data_structures/dict_combinations.py
@@ -0,0 +1,12 @@
+#CASE 1. dict in dicT
+
+d1= {
+    'no': 10,
+    'm': {'cm': 20, 'c++':21},
+    'em': {'cm': 50, 'c++':23},
+    'name': "JNTU"
+}
+print(d1)
+print(d1.items())
+print(d1.keys())
+print(d1.values())
