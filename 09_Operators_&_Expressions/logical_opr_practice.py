c4edf019013f49781dc1ee3428263cbe7d48310f
Gautam Gupta <imgautii5177@gmail.com>
2026-09-10 21:10:32 +0530

    practiced on logical operator

---
+++ 09_Operators_&_Expressions/logical_opr_practice.py
@@ -0,0 +1,33 @@
+                                        #and operator
+print("Using and Operator\n")
+#eg1
+print(True and False)
+print(False and True)
+print(False and False)
+print(True and True)
+
+#eg2
+print(12>34 and 342>23)
+print(90>20 and 100>90 and 40>23)
+print(20>10 and 100>190 and 40>23)
+print(900>100 and 100>90 and 140>230)
+
+#eg3: Special
+print(100 and 200)
+print(-200 and -300)
+print(0 and 200)
+print(100 and 0)
+print(100 and 20 and True)
+print("Java" and "Python")
+print("Java" and "Python" and " ")
+
+                                        # or operator
+#eg1
+print("Using or Operator\n")
+print(True or False)
+print(False or True)
+print(False or False)
+print(True or True)
+
+#eg2
+
