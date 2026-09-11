63ec4510313871f7b08f7ae9f5afa23eb0e6c954
Gautam Gupta <imgautii5177@gmail.com>
2026-09-11 22:40:59 +0530

    practiced on bitwise left shift operator

---
+++ 09_Operators_&_Expressions/bitwise_opr_practice.py
@@ -0,0 +1,8 @@
+                        #Bitwise left shift operator
+
+print(10<<3)
+print(4<<3)
+print(10<<0)
+print(-2<<4)
+# print(10.3<<3)  #TypeError: unsupported operand type(s) for <<: 'float' and 'int'
+# print(4<<-1)     ValueError: negative shift count
