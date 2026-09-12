557e260d195d2098b27e9eca2dfd276c5e4b0244
Gautam Gupta <imgautii5177@gmail.com>
2026-09-12 13:52:53 +0530

    added practice on bitwise operator

--- 09_Operators_&_Expressions/bitwise_opr_practice.py
+++ 09_Operators_&_Expressions/bitwise_opr_practice.py
@@ -43,3 +43,57 @@ lst1= [2,3,9]
 lst2= [8,7,9]
 print(set(lst1) | set(lst2))
 
+                                #Bitwise AND Operator
+
+#eg1
+print(1&0)
+print(0&1)
+print(0&0)
+print(1&1)
+
+#eg2
+print(10&4)
+print(5&4)
+print(20&25)
+
+#eg3
+s1={10,20,30}
+s2={30,40,50}
+s3= s1&s2
+print(s3)
+
+                            #Bitwise complement operator(~)
+
+print(12)
+print(~12)
+
+                            #Bitwise XOR Operator(^)
+
+#eg1
+print(0^0)
+print(0^1)
+print(1^0)
+print(1^1)
+
+#eg2
+print(4^5)
+print(10^15)
+print(4^6)
+
+#Swapping
+a=2
+b=3
+print(a, b)
+a=a^b
+b= a^b
+a= a^b
+print(a,b)
+
+#eg3
+s1={2,3,4}
+s2={4,5,6}
+s3= s1^s2
+print(s3)
+
+print({10,20,30}^{30,40,50})
+print({10,20}^{20,10})
