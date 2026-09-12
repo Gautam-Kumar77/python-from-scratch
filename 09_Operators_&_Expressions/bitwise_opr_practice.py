b1b5a1b7f5ad55c69d6d0ea40025173540ff27d9
Gautam Gupta <imgautii5177@gmail.com>
2026-09-12 11:40:56 +0530

    add 1 more ways to swap the values

--- 09_Operators_&_Expressions/bitwise_opr_practice.py
+++ 09_Operators_&_Expressions/bitwise_opr_practice.py
@@ -6,3 +6,13 @@ print(10<<0)
 print(-2<<4)
 # print(10.3<<3)  #TypeError: unsupported operand type(s) for <<: 'float' and 'int'
 # print(4<<-1)     ValueError: negative shift count
+
+                        #Bitwise right shift operator
+
+print(10>>3)
+print(32>>3)
+print(32>>2)
+print(20>>0)
+print(-10>>3)
+# print(12.3>>3)   TypeError: unsupported operand type(s) for >>: 'float' and 'int'
+# print(23>>-3)   ValueError: negative shift count
--- 09_Operators_&_Expressions/swapping_logics.py
+++ 09_Operators_&_Expressions/swapping_logics.py
@@ -36,3 +36,11 @@ b= a//b
 a= a//b
 print("="*40)
 print("Value of a= {} \nValue of b= {} " .format(a,b))
+print("="*40)
+#Method 5
+a=2
+b=3
+a= a^b
+b= a^b
+a= a^b
+print(a,b)
