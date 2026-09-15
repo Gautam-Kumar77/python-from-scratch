dad62023947819c45808bf9c13386f4f746e9ae4
Gautam Gupta <imgautii5177@gmail.com>
2026-09-15 15:28:26 +0530

    Practice questions on ternary operator

---
+++ 09_Operators_&_Expressions/ternary_opr_practice.py
@@ -0,0 +1,25 @@
+#1 program for accepting any value and Decide whether It is Palindrome or Not
+
+v= "12321"
+res= "Palindrome: "  if v==v[::-1]  else "Not Palindrome"
+print("{} is {}".format(v,res))
+
+st= input("Enter a value: ")
+res= "Palindrome" if st==st[::-1] else "Not a Palindrome"
+print("{} is {}".format(st, res))
+
+#2 program for accepting any Two Numerical Values and Find Biggest among them and check for Equality
+a= float(input("Enter first value: "))
+b= float(input("Enter second value: "))
+
+res= a if a>b else b  if b>a else "Both are equal"
+# print("{} is greater".format(res))
+print("Max({},{})={}".format(a,b,res))
+
+#program for accepting any Three Numerical Values and Find Biggest among them and check for Equality
+a= float(input("Enter first value: "))
+b= float(input("Enter second value: "))
+c= float(input("Enter second value: "))
+
+res= a if (a>b) and (a>c) else b if (b>a) and (b>=c) else c if (c>=a) and (c>b) else "All values are queal"
+print("Max({} {} {}) = {}".format(a,b,c,res))
