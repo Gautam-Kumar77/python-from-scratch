04f9e2ee721ba35fda8d22c1f30fb556122ddfa6
Gautam Gupta <imgautii5177@gmail.com>
2026-09-16 19:58:44 +0530

    Added Practice on string handling

--- 02_data_types/string_handling.py
+++ 02_data_types/string_handling.py
@@ -24,3 +24,22 @@ print(s.count("M"))
 s= "pYtHoN"
 print(s.swapcase())
 
+s1= "python"
+print(s1.swapcase())
+
+#5 index()
+s= "Java"
+print(s.index("J"))
+
+for i,v in enumerate(s):
+    print(i, "-->", v)
+
+s1="MISSISSIPI"
+for i,v in enumerate(s1):
+    if (v=="I"):
+        print(i, "-->", v)
+
+#6 find()   --Returns first occurence
+s="MISSISSIPI"
+print(s.find("S"))
+print(s.find("I"))
