b0af3f9991b1eb57d9bbb2c89875d84f7fa60c5d
Gautam Gupta <imgautii5177@gmail.com>
2026-09-18 22:07:44 +0530

    Added Practice on simple if statement

--- 10_Flow_Control_Statements/Conditional_Statements/simple_if_statement.py
+++ 10_Flow_Control_Statements/Conditional_Statements/simple_if_statement.py
@@ -7,7 +7,7 @@
 #     print("\tEnjoy Movie!")
 # print("Go to Home")
 #
-# #program for accepting any value and Decide whether It is Palindrome or not
+# #Ex2. program for accepting any value and Decide whether It is Palindrome or not
 # val= input("Enter any Value: ")
 # if val==val[::-1]:
 #     print("{} is Palindrome".format(val))
@@ -15,9 +15,39 @@
 #     print("{} is not Palindrome".format(val))
 # print("Program executed successfully")
 
-#Program for accepting any Numerical value and Decide whether It is +VE or -VE or Zero
-num= int(input("Enter any Numerical value: "))
-if num>0:
-    print("{} is +ve number".format(num))
-if num<0:
-    print("{} is -ve number".format(num))
+#Ex3. Program for accepting any Numerical value and Decide whether It is +VE or -VE or Zero
+# num= int(input("Enter any Numerical value: "))
+# if num>0:
+#     print("{} is +ve number".format(num))
+# if num<0:
+#     print("{} is -ve number".format(num))
+# if num==0:
+#     print("{} is Zero")
+
+#Ex4. Program for accepting any +VE Numerical Integer Value and decide whether It is Even OR Odd
+# num= int(input("Enter a number"))
+# if num<0:
+#     print("Please Enter Positive number")
+# if num>0 and num%2==0:
+#     print("Number is Positive and {} is Even Number.".format(num))
+# if num>0 and num%2!=0:
+#     print("Number is Positive but {} is Odd Number.".format(num))
+
+#Ex5. Write a Python program to calculate Simple Interest using P, T, and R. Validate that all three values are greater than zero using simple if statements and display appropriate error messages for invalid values.
+p= float(input("Enter Principle: "))
+r= float(input("Enter rate: "))
+t= float(input("Enter time: "))
+si = (p*r*t)/100
+if p>0 and r>0 and t>0:
+    print("Principle:", p)
+    print("Rate:", r)
+    print("Time:", t)
+    print("Simple Interest:", si)
+
+if p<=0:
+    print("Invalid Principle")
+if t<=0:
+    print("Invalid time")
+if r<=0:
+    print("Invalid rate")
+print("Program Execution Completed")
