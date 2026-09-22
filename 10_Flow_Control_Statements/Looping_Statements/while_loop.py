9f90f391ef51776d99d323877406e32392e5d858
Gautam Gupta <imgautii5177@gmail.com>
2026-09-22 17:23:44 +0530

    Added Practice on while loop

--- 10_Flow_Control_Statements/Looping_Statements/while_loop.py
+++ 10_Flow_Control_Statements/Looping_Statements/while_loop.py
@@ -14,3 +14,46 @@ else:
         print("="*50)
     print("Program Executed Successfully.")
 
+#Ex2: Program for Generating N to 1 where N is +VE
+n= int(input("Enter a number for Generate"))
+if n<=0:
+    print("Invalid Input")
+else:
+    i=1
+    while n>=i:
+        print(n)
+        n= n-1
+
+#Ex:3 Program for Generating all even Numbers within N
+n= int(input("Enter a number"))
+if n<=0:
+    print("Invalid Input.")
+
+else:
+    i=2
+    while i<=n:
+        print(i)
+        i=i+2
+    else:
+        print("="*50)
+
+#Ex:4 Program for Generating all even Numbers within N
+n = int(input("Enter a number:  "))
+if n<=0:
+    print("Invalid Input")
+else:
+    i=1
+    while(i<=n):
+        if i%2==0:
+            print(i)
+        i= i+1
+    print("="*50)
+print("Program Executed Successfully")
+
+#Ex5:
+n= int(input("Enter a number: "))
+while(1<=n):
+    print(n)
+    n= n-2
+else:
+    print("="*50)
