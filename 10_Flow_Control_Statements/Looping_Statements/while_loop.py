79db2f3dbee2a6efadd0109d1b4d7a30cd353414
Gautam Gupta <imgautii5177@gmail.com>
2026-09-23 19:42:53 +0530

    Practiced quesions on while loop

--- 10_Flow_Control_Statements/Looping_Statements/while_loop.py
+++ 10_Flow_Control_Statements/Looping_Statements/while_loop.py
@@ -1,74 +1,74 @@
 #Ex1: Program for Generating 1 to N where N is +VE#Program for Generating 1 to N where N is +VE
-# n= int(input("Enter +ve number: "))
-# i= 1
-#
-# if i<=0:
-#     print("Invalid Input: ")
-# else:
-#     print("="*50)
-#     print("Numbers Within:- {} ".format(n))
-#     while(i<=n):
-#         print(i)
-#         i= i+1
-#     else:
-#         print("="*50)
-#     print("Program Executed Successfully.")
-#
-# #Ex2: Program for Generating N to 1 where N is +VE
-# n= int(input("Enter a number for Generate"))
-# if n<=0:
-#     print("Invalid Input")
-# else:
-#     i=1
-#     while n>=i:
-#         print(n)
-#         n= n-1
-#
-# #Ex:3 Program for Generating all even Numbers within N
-# n= int(input("Enter a number"))
-# if n<=0:
-#     print("Invalid Input.")
-#
-# else:
-#     i=2
-#     while i<=n:
-#         print(i)
-#         i=i+2
-#     else:
-#         print("="*50)
-#
-# #Ex:4 Program for Generating all even Numbers within N
-# n = int(input("Enter a number:  "))
-# if n<=0:
-#     print("Invalid Input")
-# else:
-#     i=1
-#     while(i<=n):
-#         if i%2==0:
-#             print(i)
-#         i= i+1
-#     print("="*50)
-# print("Program Executed Successfully")
+n= int(input("Enter +ve number: "))
+i= 1
 
-#Ex5:
-# n= int(input("Enter a number: "))
-# while(1<=n):
-#     print(n)
-#     n= n-2
-# else:
-#     print("="*50)
+if i<=0:
+    print("Invalid Input: ")
+else:
+    print("="*50)
+    print("Numbers Within:- {} ".format(n))
+    while i<=n:
+        print(i)
+        i= i+1
+    else:
+        print("="*50)
+    print("Program Executed Successfully.")
 
-#Ex:6 Program for Generating all even Numbers within N
-# n=int(input("Enter How Many Numbers Even Numbers u want to Generate:"))
-# if n<=0:
-#     print("Invalid input")
-# else:
-#     i=2
-#     while i<=n:
-#         print(i)
-#         i=i+2
-#     else:
-#         print("Program Execute Successfully")
+#Ex2: Program for Generating N to 1 where N is +VE
+n= int(input("Enter a number for Generate"))
+if n<=0:
+    print("Invalid Input")
+else:
+    i=1
+    while n>=i:
+        print(n)
+        n= n-1
+
+#Ex:3 Program for Generating all even Numbers within N
+n= int(input("Enter a number"))
+if n<=0:
+    print("Invalid Input.")
+
+else:
+    i=2
+    while i<=n:
+        print(i)
+        i=i+2
+    else:
+        print("="*50)
+
+#Ex:4 Program for Generating all even Numbers within N
+n = int(input("Enter a number:  "))
+if n<=0:
+    print("Invalid Input")
+else:
+    i=1
+    while i<=n:
+        if i%2==0:
+            print(i)
+        i= i+1
+    print("="*50)
+print("Program Executed Successfully")
+
+# Ex5:
+n= int(input("Enter a number: "))
+while 1<=n:
+    print(n)
+    n= n-2
+else:
+    print("="*50)
+
+# Ex:6 Program for Generating all even Numbers within N
+n=int(input("Enter How Many Numbers Even Numbers u want to Generate:"))
+if n<=0:
+    print("Invalid input")
+else:
+    i=2
+    while i<=n:
+        print(i)
+        i=i+2
+    else:
+        print("Program Execute Successfully")
 
 #Ex:7 Program for accepting a Line of Text/word  and display every char
 s1= input("Enter a word: ")
@@ -86,3 +86,25 @@ i= -len(s1)
 while i<=-1:
     print(s1[i])
     i= i+1
+
+print("By Using while Loop in BACKWARD Direction with +VE Indices ")
+print("----------------------------------------------------")
+i= len(s1)-1
+while i>=0:
+    print(s1[i])
+    i= i-1
+
+print("By Using while Loop in BACKWARD Direction with -VE Indices ")
+print("----------------------------------------------------")
+
+i= -1
+while -len(s1)<=i:
+    print(s1[i])
+    i=i-1
+
+#Ex:8 Program for Generating Mul Table for a Given +VE Number
+table= int(input("Enter a number to print table: "))
+i=1
+while i<=10:
+    print(table, "*", i, "=", table*i)
+    i=i+1
