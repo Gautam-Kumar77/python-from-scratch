47e1c89c9e0b37bbcb170c5859c0c7afbcc0113e
Gautam Gupta <imgautii5177@gmail.com>
2026-09-25 15:15:02 +0530

    added practice on for loop

--- 10_Flow_Control_Statements/Looping_Statements/for_loop.py
+++ 10_Flow_Control_Statements/Looping_Statements/for_loop.py
@@ -1,22 +1,29 @@
-#Ex:1 Program for Generating 1 to N where N is +VE
-n= int(input("Enter a number: "))
-if n<=0:
-    print("Invalid Input!")
-else:
-    for i in range(1, n+1, 1):
-        print(i)
-    else:
-        print("="*50)
+# #Ex:1 Program for Generating 1 to N where N is +VE
+# n= int(input("Enter a number: "))
+# if n<=0:
+#     print("Invalid Input!")
+# else:
+#     for i in range(1, n+1, 1):
+#         print(i)
+#     else:
+#         print("="*50)
+#
+# #Ex:2 Program for Generating N to 1 where N is +VE
+# n= int(input("Enter a number: "))
+# for i in range(n, 0, -1):
+#     print(i)
+#
+# #Ex:3 Program for Generating Even Numbers within N is +VE
+# n= int(input("Enter a number: "))
+# if n==0:
+#     print("Invalid Input!")
+# else:
+#     for i in range(2,n+1,2):
+#         print("\t {}".format(i))
 
-#Ex:2 Program for Generating N to 1 where N is +VE
-n= int(input("Enter a number: "))
-for i in range(n, 0, -1):
-    print(i)
-
-#Ex:3 Program for Generating Eve Numbers within N is +VE
-n= int(input("Enter a number: "))
-if n==0:
-    print("Invalid Input!")
-else:
-    for i in range(2,n+1,2):
-        print("\t {}".format(i))
+#Program for Finding Sum of Digits of a +ve Number
+n= int(input("Enter a number to sum"))
+total=0
+for i in range(1, n+1, 1):
+    total= total+i
+print(total)
