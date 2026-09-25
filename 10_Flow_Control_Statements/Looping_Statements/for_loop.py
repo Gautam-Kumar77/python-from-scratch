6ccacd410265f94ce92096e1112f7410a3cd594b
Gautam Gupta <imgautii5177@gmail.com>
2026-09-25 15:55:59 +0530

    added practice on for loop

--- 10_Flow_Control_Statements/Looping_Statements/for_loop.py
+++ 10_Flow_Control_Statements/Looping_Statements/for_loop.py
@@ -21,9 +21,49 @@
 #     for i in range(2,n+1,2):
 #         print("\t {}".format(i))
 
-#Program for Finding Sum of Digits of a +ve Number
-n= int(input("Enter a number to sum"))
-total=0
-for i in range(1, n+1, 1):
-    total= total+i
-print(total)
+#Ex:4 Program for Finding Sum of Digits of a +ve Number
+# n= int(input("Enter a number to sum"))
+# total=0
+# for i in range(1, n+1, 1):
+#     total= total+i
+# print(total)
+
+#Ex:5 Program for Accepting List of Values from Key Board and display
+# n= int(input("Enter a number how many numbers you want to store: "))
+# if n<=0:
+#     print("Invalid Input!")
+# else:
+#     lst= list()
+#     for i in range(1, n+1):
+#         val= float(input("Enter {} values".format(i)))
+#         lst.append(val)
+#     else:
+#         print("Values are: ")
+#         print(lst)
+
+#Ex:6 Program for Cal sum Squares and Cubes  of N Natural Nums
+# n= int(input("Enter a number: "))
+# if n<=0:
+#     print("Invalid Input!")
+# else:
+#     total=0
+#     total1=0
+#     total2=0
+#     for i in range(1, n+1):
+#         total= total+i
+#         total1= total1 + i**2
+#         total2= total2 + i**3
+#     print("Sum of total number is: {}".format(total))
+#     print("Square of total number is: {}".format(total1))
+#     print("cube of total number is: {}".format(total2))
+#     print()
+
+#Ex:7 Program for Cal Product  of N Natural Nums
+n= int(input("Enter a number: "))
+if n<=0:
+    print("Invalid Input: ")
+else:
+    pd= 1
+    for i in range(1, n+1):
+        pd= pd*i
+    print(pd)
